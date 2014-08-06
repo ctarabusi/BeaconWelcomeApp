1fad7a37a0cd6ce90ae72abc189d41378c99d612
ctarabusi <ctarabusi@gmail.com>
2014-08-06 19:01:05 +0200

    Initial Commit

---
+++ app/src/main/java/com/ctarabusi/beacon/BeaconActivity.java
@@ -0,0 +1,131 @@
+package com.ctarabusi.beacon;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Vibrator;
+import android.util.Log;
+import android.view.Window;
+import android.widget.Toast;
+
+
+public class BeaconActivity extends Activity implements BluetoothAdapter.LeScanCallback {
+    private static final String TAG = "BeaconActivity";
+
+    private BluetoothAdapter mBluetoothAdapter;
+    private Vibrator vibrator;
+
+    private long lastDisplayed = System.currentTimeMillis();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+        setProgressBarIndeterminate(true);
+
+        /*
+         * Bluetooth in Android 4.3 is accessed via the BluetoothManager, rather than
+         * the old static BluetoothAdapter.getInstance()
+         */
+        BluetoothManager manager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
+        mBluetoothAdapter = manager.getAdapter();
+
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+        Log.i(TAG, "mBluetoothAdapter: " + mBluetoothAdapter);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        /*
+         * We need to enforce that Bluetooth is first enabled, and take the
+         * user to settings to enable it if they have not done so.
+         */
+        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
+            //Bluetooth is disabled
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivity(enableBtIntent);
+            finish();
+            return;
+        }
+
+        /*
+         * Check for Bluetooth LE Support.  In production, our manifest entry will keep this
+         * from installing on these devices, but this will allow test devices or other
+         * sideloads to report whether or not the feature exists.
+         */
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH)) {
+            Toast.makeText(this, "No LE Support.", Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+
+        //Begin scanning for LE devices
+        startScan();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //Cancel any scans in progress
+        mHandler.removeCallbacks(mStopRunnable);
+        mHandler.removeCallbacks(mStartRunnable);
+        mBluetoothAdapter.cancelDiscovery();
+    }
+
+    private Runnable mStopRunnable = new Runnable() {
+        @Override
+        public void run() {
+            stopScan();
+        }
+    };
+    private Runnable mStartRunnable = new Runnable() {
+        @Override
+        public void run() {
+            startScan();
+        }
+    };
+
+    private void startScan() {
+        //Scan for devices advertising the thermometer service
+        mBluetoothAdapter.startLeScan(this);
+        setProgressBarIndeterminateVisibility(true);
+
+        mHandler.postDelayed(mStopRunnable, 5000);
+    }
+
+    private void stopScan() {
+        mBluetoothAdapter.stopLeScan(this);
+        setProgressBarIndeterminateVisibility(false);
+
+        mHandler.postDelayed(mStartRunnable, 2500);
+    }
+
+    public void onLeScan(BluetoothDevice device, int rssi, byte[] scanRecord) {
+        Log.i(TAG, "New LE Device: " + device.getName() + " @ " + rssi + " " + scanRecord.toString());
+
+        long now = System.currentTimeMillis();
+        if (rssi > -40 && (now - lastDisplayed > 10000)) {
+            mHandler.sendMessage(Message.obtain(null, 0, "Beacon found"));
+            lastDisplayed = now;
+        }
+
+    }
+
+    private Handler mHandler = new Handler() {
+
+        public void handleMessage(Message msg) {
+            Toast.makeText(getApplicationContext(), "Welcome home!", Toast.LENGTH_LONG).show();
+            vibrator.vibrate(500);
+        }
+    };
+
+}
